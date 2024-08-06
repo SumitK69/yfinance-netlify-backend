5c8d0f7f433e45abacb65eee4a0c9ba00ae52efd
sumit khobragade <sumitbk1010@gmail.com>
2024-08-06 23:38:31 +0530

    chart min testing

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 // functions/api.js
 const express = require("express");
 const serverless = require("serverless-http");
+const yahooFinance = require("yahoo-finance2").default; // NOTE the .default
 
 const app = express();
 const router = express.Router();
@@ -10,7 +11,20 @@ router.get("/", (req, res) => {
 });
 
 router.get("/api", (req, res) => {
-  res.json({ message: "Hello from API!" });
+  res.json({ message: "Hello API!" });
+});
+router.get("/hist", async (req, res) => {
+  // const results = await yahooFinance.search("TCS.NS");
+  try {
+    const hist = await yahooFinance.chart("TCS.NS", {
+      period1: "2024-08-06",
+      interval: "1m",
+    });
+    console.log(hist);
+    res.json(hist["quotes"]);
+  } catch (error) {
+    res.json({ message: "There is an error" + error + "." });
+  }
 });
 
 app.use("/.netlify/functions/api", router);
